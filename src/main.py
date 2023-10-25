5806dc3898015b2a38420cf0e52ef1bcbf34a52f
gabriel.lion <gabriel.lion@epita.fr>
2023-10-25 21:58:16 +0200

    add poll

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import random
+import asyncio
+import aiohttp
 from utils import create_admin_role, catchphrases
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord
 
 intents = discord.Intents.default()
@@ -49,6 +51,10 @@ async def on_message(message):
         response = f"Salut tout seul, {message.author.mention}!"
         await message.channel.send(response)
     
+    if not message.author.bot:
+        author_id = message.author.id
+        message_counts[author_id] = message_counts.get(author_id, 0) + 1
+    
     await bot.process_commands(message)
 
 # Command to ban a user with an optional reason
@@ -66,6 +72,86 @@ async def ban(ctx, member: discord.Member, *, reason=None):
     else:
         await ctx.send("You don't have permission to use this command.")
 
+# Define a dictionary to store user message counts
+message_counts = {}
 
-token = "MTE2Njc4NTE5MzAxNzg3MjU1NQ.GYQg68.IdouLWMjPgeRrh6uSMWkr66bbciu2UCs1N-XqE"
+# Store flood state
+is_flooding = False
+
+# Define a warning message
+warning_message = "You're sending too many messages! Stop it or I'll ban you."
+
+# Command to (de)activate the flood monitoring
+@bot.command()
+async def flood(ctx):
+    global is_flooding
+    if is_flooding is False:
+        flood_monitor.start(ctx)
+        is_flooding = True
+        await ctx.send("Flood monitoring activated.")
+    else:
+        flood_monitor.cancel()
+        is_flooding = False
+        await ctx.send("Flood monitoring deactivated.")
+
+# Function to check for message floods
+@tasks.loop(seconds=10.0)
+async def flood_monitor(ctx):
+    guild = ctx.guild
+    for member, count in list(message_counts.items()):
+        if count >= 5:  # Adjust the threshold as needed
+            user = guild.get_member(member)
+            if user:
+                await ctx.send(f'{user.mention} {warning_message}')
+        del message_counts[member]
+
+        
+# Command to post a random xkcd comic
+@bot.command()
+async def xkcd(ctx):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://xkcd.com/info.0.json') as response:
+            if response.status == 200:
+                data = await response.json()
+                comic_num = random.randint(1, data['num'])
+                async with session.get(f'https://xkcd.com/{comic_num}/info.0.json') as comic_response:
+                    res = await comic_response.json()
+                    await ctx.send(res['img'])
+                    # if comic_response.status == 200:
+                    #     comic_data = await comic_response.json()
+                    #     embed = discord.Embed(
+                    #         title=comic_data['safe_title'],
+                    #         url=f'https://xkcd.com/{comic_data["num"]}',
+                    #         description=comic_data['alt']
+                    #     )
+                    #     embed.set_image(url=comic_data['img'])
+                    #     await ctx.send(embed=embed)
+            else:
+                await ctx.send("Sorry, I couldn't retrieve an xkcd comic at the moment.")
+
+# Command to create a poll with time limit
+@bot.command()
+async def poll(ctx, *, question):
+    poll_message = await ctx.send(f'@here {ctx.author.mention} asks: {question}')  # Mention @here
+    await poll_message.add_reaction('👍')  # Add a thumbs up reaction
+    await poll_message.add_reaction('👎')  # Add a thumbs down reaction
+
+    time_limit = 60  # Set the time limit for the poll in seconds (1 minute)
+
+    await asyncio.sleep(time_limit)
+
+    final_result = await ctx.channel.fetch_message(poll_message.id)
+    thumbs_up = 0
+    thumbs_down = 0
+
+    for reaction in final_result.reactions:
+        if reaction.emoji == '👍':
+            thumbs_up = reaction.count - 1  # Subtract 1 to exclude the bot's reaction
+        elif reaction.emoji == '👎':
+            thumbs_down = reaction.count - 1  # Subtract 1 to exclude the bot's reaction
+
+    await ctx.send(f'The poll "{question}" has ended! Final results:\n👍: {thumbs_up}\n👎: {thumbs_down}')
+    await poll_message.delete()
+
+token = "MTE2Njc4NTE5MzAxNzg3MjU1NQ.GBc3GC.9fBknewukQt7gAonbNF0oi1R5oj_r1M8r4Sw2E"
 bot.run(token)  # Starts the bot
